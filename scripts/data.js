f78a04a86ab5bcbc2de72e381ffba88395c504af
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-15 12:14:26 -0700

    added prototype and functions

--- scripts/data.js
+++ scripts/data.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var photoData = [
+  {
+    title: 'Hugo',
+    category: 'Film',
+    picUrl: '/images/001_1.JPG',
+    info: 'Photo of Hugo Diaz, Summer 2016',
+    published: '03-14-2017',
+  },
+  {
+    title: 'Milkway',
+    category: 'Digital',
+    picUrl: '/images/Milkway1.jpg',
+    info: 'Photo of the Milky Way taken at Snow Lake, WA, Summer 2016',
+    published: '03-14-2017',
+  },
+  {
+    title: 'Hugo',
+    category: 'film',
+    picUrl: '/images/001_1.JPG',
+    info: 'Photo of Hugo Diaz, Summer 2016',
+    published: '03-14-2017',
+  }
+];
