5097df2f0bd9d79ef1fc9881c893b44234e8edc4
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-20 09:44:42 -0700

    added working JS script tag and images

--- scripts/data.js
+++ scripts/data.js
@@ -21,5 +21,26 @@ var exampleSiteData = [
     siteUrl: 'https://github.com/ChristopherSClosser/fight-code',
     info: 'Final Project for Code Fellows 201',
     published: '03-03-2017',
+  },
+  {
+    title: 'Milkway',
+    category: 'photo',
+    siteUrl: 'images/Milkway1.jpg',
+    info: 'Taken Summer 2016',
+    published: '08-04-2016',
+  },
+  {
+    title: 'Cari Gill',
+    category: 'photo',
+    siteUrl: 'images/033_33.jpg',
+    info: 'Taken Summer 2016',
+    published: '07-09-2016',
+  },
+  {
+    title: 'Rachel Hubbard',
+    category: 'photo',
+    siteUrl: 'images/dreemy.jpg',
+    info: 'Taken Summer 2015',
+    published: '07-17-15',
   }
 ];
