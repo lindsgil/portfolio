6baeb061ef676f4a56e47ec68d455aa2674241c9
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-20 10:42:01 -0700

    added styling

--- scripts/siteData.js
+++ scripts/siteData.js
@@ -8,27 +8,38 @@ showAboutBox.handleAboutNav = function() {
   });
 };
 
-function ExampleSite (par) {
-  this.title = par.title;
-  this.siteUrl = par.siteUrl;
-  this.info = par.info;
-};
-
-ExampleSite.prototype.toHtml = function() {
-  var $newExampleSite = $('div.temporary').clone();
-  $newExampleSite.removeClass('temporary');
-  $newExampleSite.find('h1').html(this.title);
-  $newExampleSite.find('a').attr('href', this.siteUrl);
-  $newExampleSite.find('p').html(this.info);
-  return $newExampleSite;
-};
-
-exampleSiteData.forEach(function(siteExampleObject) {
-  sites.push(new ExampleSite(siteExampleObject));
+$('.site-nav').on('click', function() {
+  $('.main-nav').show();
 });
 
-exampleSiteData.forEach(function(a) {
-  $('#site-display').append(a.toHtml());
+$('.social-icons').on('click', function() {
+  $('.main-nav').show();
 });
+
+
+// function ExampleSite (par) {
+//   this.title = par.title;
+//   this.category = par.category;
+//   this.siteUrl = par.siteUrl;
+//   this.info = par.info;
+//   this.published = par.published;
+// };
+//
+// ExampleSite.prototype.toHtml = function() {
+//   var $newExampleSite = $('div.temporary').clone();
+//   $newExampleSite.removeClass('temporary');
+//   $newExampleSite.find('h1').html(this.title);
+//   $newExampleSite.find('a').attr('href', this.siteUrl);
+//   $newExampleSite.find('p').html(this.info);
+//   return $newExampleSite;
+// };
+//
+// exampleSiteData.forEach(function(siteExampleObject) {
+//   sites.push(new ExampleSite(siteExampleObject));
+// });
+//
+// exampleSiteData.forEach(function(a) {
+//   $('#site-display').append(a.toHtml());
+// });
 
 showAboutBox.handleAboutNav();
