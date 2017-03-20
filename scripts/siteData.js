b0079258da1d571a7a4ecc8579b1bf9b9c70be5c
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-20 11:58:29 -0700

    handlebars working and adhering to portfolio page

--- scripts/siteData.js
+++ scripts/siteData.js
@@ -1,5 +1,6 @@
+'use strict';
+
 var sites = [];
-var exampleSiteData = {};
 var showAboutBox = {};
 
 showAboutBox.handleAboutNav = function() {
@@ -16,30 +17,26 @@ $('.social-icons').on('click', function() {
   $('.main-nav').show();
 });
 
+function Site (opts) {
+  this.title = opts.title;
+  this.category = opts.category;
+  this.siteUrl = opts.siteUrl;
+  this.info = opts.info;
+  this.published = opts.published;
+};
+//
+Site.prototype.toHtml = function() {
+  var source = $('#work-display-template').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+};
 
-// function ExampleSite (par) {
-//   this.title = par.title;
-//   this.category = par.category;
-//   this.siteUrl = par.siteUrl;
-//   this.info = par.info;
-//   this.published = par.published;
-// };
-//
-// ExampleSite.prototype.toHtml = function() {
-//   var $newExampleSite = $('div.temporary').clone();
-//   $newExampleSite.removeClass('temporary');
-//   $newExampleSite.find('h1').html(this.title);
-//   $newExampleSite.find('a').attr('href', this.siteUrl);
-//   $newExampleSite.find('p').html(this.info);
-//   return $newExampleSite;
-// };
-//
-// exampleSiteData.forEach(function(siteExampleObject) {
-//   sites.push(new ExampleSite(siteExampleObject));
-// });
-//
-// exampleSiteData.forEach(function(a) {
-//   $('#site-display').append(a.toHtml());
-// });
+exampleSiteData.forEach(function(obj) {
+  sites.push(new Site(obj));
+});
+
+sites.forEach(function(a) {
+  $('#site-display').append(a.toHtml());
+});
 
 showAboutBox.handleAboutNav();
