56d114ba27e822068163654dcbee7f98addc05c8
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-20 09:29:28 -0700

    added js changes

--- scripts/siteData.js
+++ scripts/siteData.js
@@ -8,29 +8,27 @@ showAboutBox.handleAboutNav = function() {
   });
 };
 
-// function ExampleSite (par) {
-//   this.title = par.title;
-//   this.siteUrl = par.siteUrl;
-//   this.info = par.info;
-// };
-//
-// ExampleSite.prototype.toHtml = function() {
-//   var $newExampleSite = $('div.temporary').clone();
-//   $newExampleSite.removeClass('temporary');
-//   $newExampleSite.find('h1').html(this.title);
-//   $newExampleSite.find('a').attr('href', this.siteUrl);
-//   $newExampleSite.find('p').html(this.info);
-//   return $newExampleSite;
-// };
-//
-// exampleSiteData.forEach(function(siteExampleObject) {
-//   sites.push(new ExampleSite(siteExampleObject));
-// });
-//
-// exampleSiteData.forEach(function(a) {
-//   $('#site-display').append(a.toHtml());
-// });
-//
-// ExampleSite();
-// exampleSiteData.forEach();
+function ExampleSite (par) {
+  this.title = par.title;
+  this.siteUrl = par.siteUrl;
+  this.info = par.info;
+};
+
+ExampleSite.prototype.toHtml = function() {
+  var $newExampleSite = $('div.temporary').clone();
+  $newExampleSite.removeClass('temporary');
+  $newExampleSite.find('h1').html(this.title);
+  $newExampleSite.find('a').attr('href', this.siteUrl);
+  $newExampleSite.find('p').html(this.info);
+  return $newExampleSite;
+};
+
+exampleSiteData.forEach(function(siteExampleObject) {
+  sites.push(new ExampleSite(siteExampleObject));
+});
+
+exampleSiteData.forEach(function(a) {
+  $('#site-display').append(a.toHtml());
+});
+
 showAboutBox.handleAboutNav();
